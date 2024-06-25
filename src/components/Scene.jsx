0fe9de2a65bf39fdb2f9de234804eefd627c7e58
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2024-06-25 20:45:00 +0100

    added footer and changed fonts

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -68,8 +68,26 @@ const container = useRef()
 
 </Canvas>
 
- <div className="invisible sm:visible absolute top-0 right-0  h-[100%] lg:w-[30vw] md:w-[40vw] border-primary  border-2 flex justify-center items-end z-10">
-      <img src="/icons/icons8-play-pause-48.png" className=" m-20 hover:cursor-pointer bg-primary hover:bg-background"/>
+ <div className="invisible sm:visible absolute top-0 right-0  h-[100%] lg:w-[32vw] md:w-[40vw] border-primary  border-2 flex justify-center items-end z-10">
+      {/* <img src="/icons/icons8-play-pause-48.png" /> */}
+          <svg className="m-20 hover:cursor-pointer hover:scale-110 transition-all duration-500 hover:ease-in-out fill-primary  border-2 rounded-lg p-2 border-primary " version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+            width="40px" height="40px" viewBox="0 0 45.974 45.975"
+            xml:space="preserve">
+            <g>
+              <g>
+                <g>
+                  <path d="M9.629,44.68c-1.154,1.16-2.895,1.51-4.407,0.885c-1.513-0.623-2.5-2.1-2.5-3.735V4.043c0-1.637,0.987-3.112,2.5-3.736
+                    c1.513-0.625,3.253-0.275,4.407,0.885l17.862,17.951c2.088,2.098,2.088,5.488,0,7.585L9.629,44.68z"/>
+                </g>
+                <g>
+                  <g>
+                    <path d="M38.252,45.975c-2.763,0-5-2.238-5-5V5c0-2.762,2.237-5,5-5c2.762,0,5,2.238,5,5v35.975
+                      C43.252,43.736,41.013,45.975,38.252,45.975z"/>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
  </div>
 
 
